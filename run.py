fc18cdb3af8fedbcd198536362b148ea1b216136
Tati <tati@Tatis-MacBook-Pro.local>
2015-03-01 21:21:46 -0500

    initial commit

---
+++ run.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+def findAllFiles( fileDirectory, fileExtension ):
+	returnFiles = []
+	if os.path.isdir(fileDirectory):
+		for FILE in os.listdir( fileDirectory ):
+			print FILE
+			if str( FILE ).endswith( fileExtension ):
+				returnFiles.append( str( FILE ).replace( '.' + fileExtension, '' ) )
+
+		print returnFiles
+		return returnFiles
+	else:
+		print "not a directory"
+
+print findAllFiles("/", ".txt")
+
